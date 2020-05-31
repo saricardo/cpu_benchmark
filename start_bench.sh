27df45db1867054e0f6edf9976a9e1c9b16c1d9f
RicardoSa <saricardo@example.org>
2020-05-30 18:55:11 -0700

    Feature: script waits until pid starts

--- start_bench.sh
+++ start_bench.sh
@@ -2,9 +2,12 @@
 
 if [[ $# -lt 1 || $# -gt 2 ]]; then
 	echo "Wrong Usage"
+	echo "Usage ./start_bench.sh <MODE> <PROCESS>[OPTIONAL]"
 	exit 1
 
 fi
+
+sudo apt-get install gnuplot > /dev/null 2>&1 || sudo apt-get install gnuplot
 
 #create folder with date and name
 
@@ -17,10 +20,19 @@ mode=$1
 proccess=$2
 fi
 
+if [[ -z "$proccess"  ]]; then
+    echo "Starting Monitoring"
+else
+	until pid=$(pidof -s $proccess)
+	do  
+    		echo "Waiting for $proccess to start"
+    		echo ".." 
+    		sleep 1
+	done
+	echo "Starting Monitoring"
+fi
 
 
-pid=$(pidof -s $proccess)
-##TODO wait for proccess start
 
 cd build/
 
